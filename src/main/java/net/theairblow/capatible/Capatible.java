83dfca99b92b1a85fa15753067eaf733cb8c4ff5
TheAirBlow <pilexet.danila@gmail.com>
2022-12-30 15:35:47 +0500

    Replace reflection with annotation-detector

--- src/main/java/net/theairblow/capatible/Capatible.java
+++ src/main/java/net/theairblow/capatible/Capatible.java
@@ -1,5 +1,7 @@
 package net.theairblow.capatible;
 
+import eu.infomas.annotation.AnnotationDetector;
+import eu.infomas.annotation.Cursor;
 import net.minecraft.command.ICommand;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.capabilities.Capability;
@@ -18,23 +20,18 @@ import net.theairblow.capatible.data.CapabilityFactory;
 import net.theairblow.capatible.data.CapabilityStorage;
 import net.theairblow.capatible.data.ICapabilityHolder;
 import org.apache.logging.log4j.Logger;
-import org.reflections.Configuration;
-import org.reflections.Reflections;
-import org.reflections.scanners.Scanner;
 
-import java.net.URL;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.Set;
-import java.util.function.Predicate;
 
 @Mod(modid = Capatible.MOD_ID, name = Capatible.MOD_NAME, version = Capatible.VERSION)
 public class Capatible {
-    private final List<Class<?>> commands = new ArrayList<>();
+    private static final List<Class<?>> commands = new ArrayList<>();
     public static final String MOD_ID = "capatible";
     public static final String MOD_NAME = "Capatible!";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.0.2";
     public static Logger LOGGER;
 
     @CapabilityInject(ICapabilityHolder.class)
@@ -67,35 +64,44 @@ public class Capatible {
 
     /**
      * Handles annotated classes in your mod.
-     * Currently it only handles @EventHandlerClass.
+     * Currently it handles annotations only.
      */
-    public void handleMod(String modId) {
+    public static void handleMod(String modId) {
         Optional<ModContainer> mod = Loader.instance().getModList().stream()
                 .filter(x -> x.getModId().equals(modId)).findFirst();
         if (!mod.isPresent()) throw new IllegalArgumentException("Invalid mod ID!");
-        Reflections ref = new Reflections(mod.get().getMod().getClass().getPackage().getName());
-        Set<Class<?>> classes = ref.getTypesAnnotatedWith(EventHandler.class);
-        for (Class<?> cl : classes) {
-            LOGGER.info("Registering event handler for {}", cl.getCanonicalName());
-            MinecraftForge.EVENT_BUS.register(cl);
-        }
-
-        classes = ref.getTypesAnnotatedWith(net.theairblow.capatible.annotations.Capability.class);
-        for (Class<?> cl : classes) {
-            LOGGER.info("Registering capability {}", cl.getCanonicalName());
-            try {
-                CapabilityRegistry.register((ICapability) cl.newInstance());
-            } catch (InstantiationException | IllegalAccessException e) {
-                Capatible.LOGGER.error("Failed to register capability {}", cl.getCanonicalName());
-                e.printStackTrace();
-
-                Capatible.LOGGER.error("THIS IS A SERIOUS PROBLEM! DO NOT IGNORE! " +
-                        "IT MEANS THAT A MOD THAT IS USING CAPATIBLE IS BROKEN, " +
-                        "SO REPORT THIS ISSUE TO DEVS OF THAT MOD IMMEDIATELY!");
+        try {
+            List<Class<?>> classes = AnnotationDetector.scanClassPath(
+                    mod.get().getMod().getClass().getPackage().getName())
+                    .forAnnotations(EventHandler.class).collect(Cursor::getType);
+            for (Class<?> cl : classes) {
+                LOGGER.info("Registering event handler for {}", cl.getCanonicalName());
+                MinecraftForge.EVENT_BUS.register(cl);
             }
-        }
 
-        classes = ref.getTypesAnnotatedWith(Command.class);
-        commands.addAll(classes);
+            classes = AnnotationDetector.scanClassPath(mod.get().getMod().getClass().getPackage().getName())
+                    .forAnnotations(net.theairblow.capatible.annotations.Capability.class).collect(Cursor::getType);
+            for (Class<?> cl : classes) {
+                LOGGER.info("Registering capability {}", cl.getCanonicalName());
+                try {
+                    CapabilityRegistry.register((ICapability) cl.newInstance());
+                } catch (InstantiationException | IllegalAccessException e) {
+                    Capatible.LOGGER.error("Failed to register capability {}", cl.getCanonicalName());
+                    e.printStackTrace();
+
+                    Capatible.LOGGER.error("THIS IS A SERIOUS PROBLEM! DO NOT IGNORE! " +
+                            "IT MEANS THAT A MOD THAT IS USING CAPATIBLE IS BROKEN, " +
+                            "SO REPORT THIS ISSUE TO DEVS OF THAT MOD IMMEDIATELY!");
+                }
+            }
+
+            classes = AnnotationDetector.scanClassPath(
+                    mod.get().getMod().getClass().getPackage().getName())
+                    .forAnnotations(Command.class).collect(Cursor::getType);
+            commands.addAll(classes);
+        } catch (Exception e) {
+            Capatible.LOGGER.error("Failed to process annotations for " + modId);
+            e.printStackTrace();
+        }
     }
 }
